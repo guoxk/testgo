f25e6f95a1e8e55b5bbd4c133e66a55a77785d0c
gxk <guoxiaokai@hydelive.com>
2024-01-17 11:09:27 +0800

    test

---
+++ out.go
@@ -0,0 +1,20 @@
+// Package testgo
+// @Title        out.go
+// @Description
+// @Author       gxk
+// @Time         2024/1/17 11:01 AM
+package testgo
+
+import "fmt"
+
+const (
+	AA = 1
+)
+
+type ATest struct {
+	A int
+}
+
+func Afunc() {
+	fmt.Println("Afunc")
+}
