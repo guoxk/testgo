f8fcb0840e0df3f6fa560d51c8136981567e37d2
gxk <guoxiaokai@hydelive.com>
2024-01-17 11:13:38 +0800

    test

---
+++ internal1/define/define.go
@@ -0,0 +1,20 @@
+// Package define
+// @Title        define.go
+// @Description
+// @Author       gxk
+// @Time         2024/1/17 10:59 AM
+package define
+
+import "fmt"
+
+const (
+	AA = 1
+)
+
+type ATest struct {
+	A int
+}
+
+func Afunc() {
+	fmt.Println("Afunc")
+}
---
+++ internal1/test.go
@@ -0,0 +1,20 @@
+// Package internal
+// @Title        test.go
+// @Description
+// @Author       gxk
+// @Time         2024/1/17 11:01 AM
+package internal
+
+import "fmt"
+
+const (
+	AA = 1
+)
+
+type ATest struct {
+	A int
+}
+
+func Afunc() {
+	fmt.Println("Afunc")
+}
